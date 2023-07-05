123b0a65b8821bf20c719df237e47ab0c7930622
NizamX64 <nizambhagaskara0401@gmail.com>
2023-07-05 14:30:25 +0700

    First Commit

---
+++ Introduction/hello.c
@@ -0,0 +1,10 @@
+// Including (copy and paste) a library
+#include <stdio.h>
+
+int main() {
+ // Printing "Hello World" to console
+ printf("Hello World!");
+
+ // I've defined SUCCESS in stdio.h
+ return SUCCESS;
+}
